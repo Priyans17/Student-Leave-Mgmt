807e209fb052449ce2125a0c5a101b8056e0fc69
Priyansh <priyans.mehta1712@gmail.com>
2023-06-02 00:56:05 +0530

    first commit

---
+++ studentleave.js
@@ -0,0 +1,7 @@
+function Others2(val){
+ var element=document.getElementById('others2');
+ if(val=='Type of Leave'||val=='Other2')
+   element.style.display='block';
+ else  
+   element.style.display='none';
+}
