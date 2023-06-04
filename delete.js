4e0ef73d489ca605234823f551c700bf7dbded1a
Priyansh mehta <120036015+Priyans17@users.noreply.github.com>
2023-06-04 18:09:24 +0530

    Add files via upload

---
+++ delete.js
@@ -0,0 +1,34 @@
+const dialog = document.querySelector('dialog'),
+  invoke = document.getElementById('invoke'),
+  output = document.querySelector('output'),
+  cancel = dialog.querySelector('.cancel');
+
+if (typeof dialog.showModal !== 'function') {
+  output.value = `Modal dialog not supported`;
+}
+
+invoke.addEventListener('click', () => {
+  dialog.returnValue = false;
+  dialog.showModal();
+});
+
+dialog.addEventListener('close', () => {
+  dialog.returnValue = dialog.returnValue.toLowerCase() === 'true';
+  output.value = dialog.returnValue;
+});
+
+cancel.addEventListener('click', () => {
+  dialog.close(false);
+});
+
+dialog.addEventListener('click', (e) => {
+  const rect = dialog.getBoundingClientRect();
+
+  const inDialog =
+    rect.top <= e.clientY &&
+    e.clientY <= rect.top + rect.height &&
+    rect.left <= e.clientX &&
+    e.clientX <= rect.left + rect.width;
+
+  !inDialog && dialog.close();
+});
