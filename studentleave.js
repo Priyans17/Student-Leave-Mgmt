2a06d29981e82c3ab8da409cff5cee810f4595b1
Priyansh mehta <120036015+Priyans17@users.noreply.github.com>
2023-06-04 18:07:06 +0530

    Delete studentleave.js

--- studentleave.js
+++
@@ -1,7 +0,0 @@
-function Others2(val){
- var element=document.getElementById('others2');
- if(val=='Type of Leave'||val=='Other2')
-   element.style.display='block';
- else  
-   element.style.display='none';
-}
